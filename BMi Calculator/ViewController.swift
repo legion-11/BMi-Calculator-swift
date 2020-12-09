72391998b7acd7ba30fb9ccdb054dfa7b2012584
legion-11 <legalionion@gmail.com>
2020-12-09 22:15:20 +0200

    Initial Commit

---
+++ BMi Calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BMi Calculator
+//
+//  Created by legion-11 on 09.12.2020.
+//  Copyright © 2020 legion-11. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
